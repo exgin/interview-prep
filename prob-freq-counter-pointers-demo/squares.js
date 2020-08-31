f9c5d0efb27fb7d41ec7759c9e3ef4afbf856801
unknown <exgin3538@yahoo.com>
2020-08-31 15:43:26 -0400

    finish validAnagram()

--- prob-freq-counter-pointers-demo/squares.js
+++ prob-freq-counter-pointers-demo/squares.js
@@ -61,25 +61,33 @@ Anagram- a word, phrase, or name formed by rearranging the letters of another
 */
 function createFrequencyCounterStr(str) {
   const freq = new Map();
+
   for (let el of str) {
-    freq[el] = freq[el] + 1 || 1;
+    let elCount = freq.get(el) || 0;
+    freq.set(el, elCount + 1);
   }
   return freq;
 }
 
 function validAnagram(str1, str2) {
+  // we don't even want to compare if the length is off
+  if (str1.length !== str2.length) return false;
+
   // 1. get frequcny of letters in both strings
   let str1Freq = createFrequencyCounterStr(str1);
   let str2Freq = createFrequencyCounterStr(str2);
 
   console.log(str1Freq);
-  // why are these values returning undefined & 0, when there is a Map
-  console.log(str1Freq.get('b'));
-  console.log(str1Freq.size);
 
-  // 2. compare if the key value's are the sam
-  for (let key of str1Freq.keys()) {
+  // now if the size's Map's size is off, its automatically false
+  if (str1Freq.size !== str1Freq.size) return false;
+
+  // 2. compare if the key value's are the same
+  for (let ltr of str1Freq.keys()) {
+    // comapre the key's values
+    if (str2Freq.get(ltr) !== str1Freq.get(ltr)) return false;
   }
+  return true;
 }
 
-console.log(validAnagram('bob', 'bill'));
+console.log(validAnagram('bob', 'bob'));
