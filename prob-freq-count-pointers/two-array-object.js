9796ed72f9af87cef3e8c345ab94bf4a987a4a43
unknown <exgin3538@yahoo.com>
2020-09-02 10:20:19 -0400

    two arry & range problem

--- prob-freq-count-pointers/two-array-object.js
+++ prob-freq-count-pointers/two-array-object.js
@@ -1,11 +1,21 @@
 /* turn an array to an object
 
-1) if there are not enough values, the rest of the keys value is null
+1) if there are not enough values for keys, the rest of the keys value is null
 2) if there is not enough keys, ignore the rest of the values
 
 */
 function twoArrayObject(keys, values) {
-  let ds = new Map(keys, values);
+  const newMap = new Map();
+
+  return keys.reduce((obj, cur, idx) => {
+    /* 
+    when the idx of keys is less than our value's length | set the value to the idx value
+    else its null
+    */
+    obj[cur] = idx < values.length ? values[idx] : null;
+
+    return obj;
+  }, {});
 }
 
 console.log(twoArrayObject(['a', 'b', 'c', 'd'], [1, 2, 3, 4]));
---
+++ rangeNum.js
@@ -0,0 +1,30 @@
+/* 
+Input: 13
+Output: thirten
+--
+Input: 42
+Output: forty two
+--
+Input: 194
+Output: one hundred ninety four
+--
+Input: 5421
+Output: one thousand four hundread twenty one
+--
+Input: 78934
+Output: seventy eight thousand nine hundread thirty four
+--
+Anything over 99,999 is invalid
+*/
+
+const base = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const teens = ['ten', 'eleven', 'twelve', 'thriteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'ninteen'];
+const baseTens = ['twenty', 'thirty', 'fourty', 'fiffty', 'sixty', 'seventy', 'eigthy', 'ninety'];
+
+function splitNum(num) {
+  // split the number
+
+  return num;
+}
+
+console.log(splitNum(13));
