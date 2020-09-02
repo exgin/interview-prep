3f078aab906425f99322a16b7556f618050e25d7
unknown <exgin3538@yahoo.com>
2020-09-02 12:30:45 -0400

    finish pivotIndex & sameFreq

--- prob-freq-count-pointers/pivot-index.js
+++ prob-freq-count-pointers/pivot-index.js
@@ -1,3 +1,45 @@
-// add whatever parameters you deem necessary
-function pivotIndex() {
+/* return the pivot index where the sum of the nums to the left, equal to the sums of the right
+if there are more than one valid pivot index, return the smallest
+
+[1,2,1,6,3,1] => 3 | 1,2,1 = 4 & 3,1 = 4
+[5,2,7] -1 | no valid pivot
+*/
+function pivotIndex(nums) {
+  let rightSum = nums.reduce((a, b) => a + b); // aka, the total sum of the array
+  console.log(rightSum);
+  let leftSum = 0;
+  let pivotIndex = -1;
+
+  for (let i = 0; i < nums.length; i++) {
+    leftSum += nums[i];
+
+    // when the left & right equal eachother, return that index
+    if (leftSum === rightSum) {
+      pivotIndex = i;
+      break;
+    }
+
+    rightSum -= nums[i];
+    console.log(rightSum);
+  }
+  return pivotIndex;
 }
+
+console.log(pivotIndex([1, 3, 1]));
+
+/* starting version */
+function pivotIndexV1(nums, start = 0, end = nums.length - 1) {
+  // maybe start from oppositve ends of the array and keep adding each value
+
+  for (start; start < nums.length; start++) {
+    let leftSum = (nums[start] += nums[start + 1]);
+    console.log(leftSum);
+  }
+
+  for (end; end >= 0; end--) {
+    let rightSum = (nums[end] += nums[end - 1]);
+    console.log(rightSum);
+  }
+}
+
+console.log(pivotIndexV1([1, 2, 1, 6, 3, 1]));
--- prob-freq-count-pointers/same-frequency.js
+++ prob-freq-count-pointers/same-frequency.js
@@ -1,3 +1,34 @@
-// add whatever parameters you deem necessary
-function sameFrequency() {
+/* Given two positive integers, find out if the two numbers have the same frequency of digits 
+11, 111 => false
+321, 123 => true
+34,14 => false
+*/
+function sameFrequency(n1, n2) {
+  let n1Str = n1.toString();
+  let n2Str = n2.toString();
+  if (n1Str.length !== n2Str.length) return false;
+
+  // get frequencies
+  let n1Freq = freqCounter(n1Str);
+  let n2Freq = freqCounter(n2Str);
+
+  // compare if they match up
+  for (let k of n1Freq) {
+    if (n1Freq.get(k) === n2Freq.get(k)) return true;
+  }
+  return false;
 }
+
+function freqCounter(nums) {
+  const freq = new Map();
+
+  // turn our nums to a string which allows us to iterate over it
+  for (let k of nums.toString()) {
+    let kCounter = freq.get(k) || 0;
+    freq.set(k, kCounter + 1);
+  }
+
+  return freq;
+}
+
+console.log(sameFrequency(7654321, 1234567));
